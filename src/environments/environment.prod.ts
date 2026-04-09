b8989381613e924cd248190b400f5833f1e53946
Mohamedarsath <30947480+Mohamedarsath@users.noreply.github.com>
2026-04-09 22:48:29 +0530

    Delete src directory

--- src/environments/environment.prod.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: true,
-  serverUrl: 'https://video-translator-server.onrender.com',
-  appSecret: 'translator_secure_access_2026' // Matching the backend key
-};
--- src/environments/environment.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: false,
-  serverUrl: 'http://localhost:3000',
-  appSecret: 'translator_secure_access_2026'
-};
